0b17093a234fa50dd749873ef46fb2813b907fd3
unknown <p.mwtr99@gmail.com>
2025-03-28 01:50:42 +0700

    check backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,11 @@ app.use('/api', apiRoutes);
 // Seed initial data
 seedAnswers();
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
