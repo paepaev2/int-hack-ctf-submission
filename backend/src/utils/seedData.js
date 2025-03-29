7b6b2dea0ad55d57ebedba9380ef4c7b15b3d246
unknown <p.mwtr99@gmail.com>
2025-03-29 18:33:09 +0700

    fix: PROBLEM 4

--- backend/src/utils/seedData.js
+++ backend/src/utils/seedData.js
@@ -4,7 +4,7 @@ const CORRECT_ANSWERS = {
   'PROBLEM 1': 'SPAGHETTI',
   'PROBLEM 2': 'CONAN',
   'PROBLEM 3': 'Hello_web_developer',
-  'PROBLEM 4': '08934821',
+  'PROBLEM 4': '08937821',
   'PROBLEM 5': 'HTTP_POST_the_flAg',
   'PROBLEM 6': 'i_XOR_y0u',
   'PROBLEM 7': 'ILOVEDOG',
